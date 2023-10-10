b2a727b3359cc8a6269f245fcd3e4d63c293f399
ᴀᴅɪᴛʏᴀ ᴊʜᴀ <119198739+PyAaditya@users.noreply.github.com>
2023-10-10 14:06:58 +0530

    Update must_join.py

--- StringSessionBot/must_join.py
+++ StringSessionBot/must_join.py
@@ -13,16 +13,16 @@ async def must_join_channel(bot: Client, msg: Message):
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+                link = "https://t.me/ZenBotX"
             else:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"You must join [this channel](https://t.me/ZenBotX) to use me. After joining try again !",
+                    f"You must join [this channel]({link}) to use me. After joining try again !",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url="https://t.me/ZenBotX")]
+                        [InlineKeyboardButton("ᴄʜᴀɴɴᴇʟ", url={link})]
                     ])
                 )
                 await msg.stop_propagation()
